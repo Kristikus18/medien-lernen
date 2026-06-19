04ebd09069909418507fa4ae4ebdb66047438d26
Lozova Krystyna <kristilozova@gmail.com>
2026-06-19 15:36:48 +0200

    Add iPad art lesson tracker

---
+++ ipad-art/sw.js
@@ -0,0 +1,43 @@
+const cacheName = "ipad-art-v1";
+const files = [
+  "./",
+  "./index.html?v=1",
+  "./styles.css?v=1",
+  "./app.js?v=1",
+  "./manifest.webmanifest?v=1",
+  "../icon-192.png",
+  "../icon-512.png",
+  "../apple-touch-icon.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => cache.addAll(files))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key.startsWith("ipad-art-") && key !== cacheName).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(cacheName).then((cache) => cache.put(event.request, copy));
+        return response;
+      });
+    })
+  );
+});
